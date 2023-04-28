e7f299b029bb1bc56b83084cac1b828bc986bbb6
Zeph180 <zephrichards1@gmail.com>
2023-04-29 00:23:00 +0300

    Add logo and hamburger

---
+++ src/assets/hamburg.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+const Hamburger = (props) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={25}
+		height={25}
+		fill="none"
+		{...props}
+	>
+		<path stroke="#0E7EE7" strokeWidth={5} d="M0 2.5h25M0 12.5h25M0 22.5h25" />
+	</svg>
+);
+export default Hamburger;

212bb4ac49d642aa97a94e21a5b557b13d277cc7
Zeph180 <zephrichards1@gmail.com>
2023-04-29 00:24:20 +0300

    Add section navigation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage";
 import Navbar from "./components/navbar/Navbar";
 import ProjectsPage from "./pages/ProjectsPage";
-import ContactPage from "./pages/ContactPage";
+// import ContactPage from "./pages/ContactPage";
 import "./App.css";
 // import darkBg from "./assets";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<div id="app-container">
-				<Navbar />
-				<Routes>
-					<Route exact path="/" element=<HomePage /> />
-					<Route path="/about" element=<AboutPage /> />
-					<Route path="/projects" element=<ProjectsPage /> />
-					<Route path="/contact" element=<ContactPage /> />
-					{/* <Route path="/resume" element=<AboutPage /> /> */}
-				</Routes>
-			</div>
+	// <BrowserRouter>
+	// 	<div id="app-container">
+	// 		<Navbar />
+	// 		<Routes>
+	// 			<Route exact path="/" element=<HomePage /> />
+	// 			<Route path="/about" element=<AboutPage /> />
+	// 			<Route path="/projects" element=<ProjectsPage /> />
+	// 			<Route path="/contact" element=<ContactPage /> />
+	// 			{/* <Route path="/resume" element=<AboutPage /> /> */}
+	// 		</Routes>
+	// 	</div>
 
-		</BrowserRouter>
+	// </BrowserRouter>
+		<div id="app-container">
+			<Navbar />
+			<HomePage />
+			<AboutPage />
+			<ProjectsPage />
+		</div>
 	);
 }
 

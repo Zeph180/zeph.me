adceed4486c9ab2384799a6eaaafcfb5eaad9e01
Zeph180 <zephrichards1@gmail.com>
2023-04-29 17:20:59 +0300

    Conditionaly render list

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import CloseBtn from "../../assets/CloseBtn";
 import Hamburger from "../../assets/hamburg";
 import MyLogo from "../../assets/MyLogo";
 // import { Link,} from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+	const [isListShown, setIsListShown] = useState(false);
+	const toggleListShown = () => {
+
+		setIsListShown(!isListShown);
+	};
+
 	return (
 		<nav>
 			<div id="nav-container">
-				<MyLogo />
-				<Hamburger />
-				<ul id="nav-list">
+				<MyLogo onClick={() => alert("Clicked")}/>
+				<div>{isListShown ? <CloseBtn onClick={toggleListShown}/> : <Hamburger onClick={toggleListShown}/>}</div>
+				<ul id="nav-list" className={!isListShown ? "hidden" : null}>
 					<li className="nav-list-item">
 						<a href="#home-container">Home</a>
-					</li>	
+					</li>
 					<li className="nav-list-item">
 						<a href="#about">About</a>
 					</li>
